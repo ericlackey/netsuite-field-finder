2c928693327094799f460ddaff7894b52412ab58
Eric Lackey <eric.lackey@daveramsey.com>
2022-10-19 22:51:52 -0500

    major updates

--- scripts/content.js
+++ scripts/content.js
@@ -1,83 +1,13 @@
 /*
 *
-* This script handles injecting code into the site and handling
-* saving settings into Chrome storage
+* This script handles injecting code into the site
 *
 */
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: false, childList: true, subtree: true };
-
-// Wait for fieldFilter element to be added to main page and then load settings and add event handlers to save settings
-const callback = (mutationList, observer) => {
-  for (const mutation of mutationList) {
-    if (mutation.addedNodes.length >0 && mutation.addedNodes[0].id === 'fieldFilter') {
-        loadSettingsFromStorage();
-        addEventHandlers();
-    }
-  }
-};
-
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(document, config);
-
-// Inject code into the main page
+// Inject script into the main page
 var s = document.createElement('script');
 s.src = chrome.runtime.getURL('scripts/ns_ss_fieldfilter.js');
 s.onload = function() {
     this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
-
-// On load, load the cached values from Chrome storage
-function loadSettingsFromStorage() {
-    chrome.storage.sync.get([
-        'ff_show_custom_body_fields',
-        'ff_show_custom_column_fields',
-        'ff_show_related_table_fields',
-        'ff_show_native_fields',
-        'ff_show_search_input'
-        ], function(data) {
-        document.getElementById('ff_show_custom_body_fields').checked = data.ff_show_custom_body_fields;
-        document.getElementById('ff_show_custom_column_fields').checked = data.ff_show_custom_column_fields;
-        document.getElementById('ff_show_related_table_fields').checked = data.ff_show_related_table_fields;
-        document.getElementById('ff_show_native_fields').checked = data.ff_show_native_fields;
-        document.getElementById('ff_show_search_input').value = data.ff_show_search_input;
-    });
-}
-
-// Find all of the Field Field field elements and add a change listener on them
-function addEventHandlers() {
-    document.querySelectorAll("[id^='ff_show_']").forEach((el) => {
-        el.addEventListener('change', handleFieldFilterChange);
-    });
-}
-
-// Save any settings changes to Chrome storage
-function handleFieldFilterChange(event) {
-    const element = event.target;
-    let syncObject = {}
-    syncObject[element.id] = element.getAttribute('type') == 'checkbox' ? element.checked : element.value;
-    chrome.storage.sync.set(syncObject);
-}
-
-// Listen for Chrome Storage changes so we can update the Field Filter fields if they were changed in iFRAME
-chrome.runtime.onMessage.addListener((changes) => {
-    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-        let el = document.getElementById(key);
-        let elementType = el.getAttribute("type");
-        if (elementType == "checkbox" && el.checked != newValue) {
-            el.checked = newValue;
-        }
-        else if (elementType == "text" && el.value != newValue) {
-            el.value = newValue;
-        }
-        // We must dispatch an event ourselves so main page knows something changed
-        var event = new Event('change');
-        el.dispatchEvent(event);
-    }
-    return true
-});
--- scripts/ns_ss_fieldfilter.js
+++ scripts/ns_ss_fieldfilter.js
@@ -7,7 +7,7 @@
 
 
 // Define the Saved Search fields where we want to add filtering capability
-const fieldsToFilter = "input[id^='inpt_filterfilter'],input[id^='inpt_field'],input[id^='inpt_rffield'],input[id^='inpt_fffilter']";
+const fieldsToFilter = "input[id^='inpt_filterfilter'],input[id^='inpt_field'],input[id^='inpt_rffield'],input[id^='inpt_fffilter'],input[id^='inpt_sort']";
 
 // Prepare the dropdowns for Field Filter
 document.querySelectorAll(fieldsToFilter).forEach((el) => {
@@ -15,101 +15,18 @@ document.querySelectorAll(fieldsToFilter).forEach((el) => {
     return true;
 });
 
-// Add the Field Filter settings elements
-addFieldFilterElement();
-
-// Filter the dropdowns based on current settings
-filterDropdowns();
-
-// Find all of the Field Filter field elements and add an event listener
-document.querySelectorAll("[id^='ff_show_']").forEach((el) => {
-    el.addEventListener('change', filterDropdowns);
-});
-
-// Add the Field Filter form elements to the page
-function addFieldFilterElement () {
-
-    // Create the Field Filter Element on the page
-    const fieldFilterDiv = document.createElement('div');
-    fieldFilterDiv.setAttribute('onmouseleave','this.style.display="none"'); // Hide the div when the mouse leaves
-    fieldFilterDiv.id = 'fieldFilter';
-    fieldFilterDiv.classList.add('uir-tooltip');
-    fieldFilterDiv.classList.add('uir-field-tooltip-wrapper');
-    fieldFilterDiv.style.cssText = 'top: 250px; padding: 20px; left: 500px; width: 400px; height: 150px; display: none;';
-
-    const customBodyFieldsCheckbox = document.createElement('div');
-    const customColumnFieldsCheckbox = document.createElement('div');
-    const relatedTableFieldsCheckbox = document.createElement('div');
-    const nativeFieldsCheckbox = document.createElement('div');
-    const searchInputField = document.createElement('div');
-    searchInputField.setAttribute('id','woohooo');
-
-    customBodyFieldsCheckbox.innerHTML = `<input type="checkbox" id="ff_show_custom_body_fields" checked> Custom Body Fields`;
-    customColumnFieldsCheckbox.innerHTML = `<input type="checkbox" id="ff_show_custom_column_fields" checked> Custom Column Fields`;
-    relatedTableFieldsCheckbox.innerHTML = `<input type="checkbox" id="ff_show_related_table_fields" checked> Related Table Fields`;
-    nativeFieldsCheckbox.innerHTML = `<input type="checkbox" id="ff_show_native_fields" checked> Native Fields`;
-    searchInputField.innerHTML = `Field Name or ID Includes&nbsp;<input type="text" id="ff_show_search_input" value="">`;
-    searchInputField.style.cssText="padding-top: 10px;"
-
-    fieldFilterDiv.appendChild(customBodyFieldsCheckbox);
-    fieldFilterDiv.appendChild(customColumnFieldsCheckbox);
-    fieldFilterDiv.appendChild(relatedTableFieldsCheckbox);
-    fieldFilterDiv.appendChild(nativeFieldsCheckbox);
-    fieldFilterDiv.appendChild(searchInputField);
-    document.body.appendChild(fieldFilterDiv);
-
-    // Determine if we are inside an iFrame or not
-    if ( window.location !== window.parent.location ) {
-        const fieldSelector = document.getElementById('inpt_field2');
-        if (fieldSelector) {
-            // Add a link to open the Field Filter beneath the field dropdown
-            const fieldSelectorParent = document.getElementById('inpt_field2').parentNode;
-            const newDiv = document.createElement("div");
-            newDiv.innerHTML = `<a href="#" id="fieldfilter_iframe" onclick="displayFieldFilterSettings(this);">Field Filter</a>`;        
-            fieldSelectorParent.appendChild(newDiv);
-        }
-    } else {
-        const tabs = document.getElementById('tb');
-        if (tabs) {
-            // Add a link to open the Field Filter on the search main menu
-            const row = tabs.rows[0];
-            const cell = row.insertCell(row.cells.length-2);
-            cell.setAttribute('valign','middle');
-            cell.setAttribute('nowrap',null);
-            cell.id = 'fieldfilter_tablnk';
-            cell.style.className='formtabloff';
-            cell.innerHTML=`<a href="#" id="fieldfilter_main" onclick="displayFieldFilterSettings(this);" class="formtabtext formtabtextoff">Field Filter</a>`;        
-        }
-    }
-
-}
-
-function displayFieldFilterSettings(element) {
-    var fieldFilterElement=document.getElementById('fieldFilter');
-    if (fieldFilterElement.style.display == 'block') {
-        fieldFilterElement.style.display='none';
-        return false;
-    };
-    var fieldFilterLocation = element.getBoundingClientRect();
-    var leftPx = parseInt(fieldFilterLocation.x).toString() + 'px';
-    var topPx = parseInt(fieldFilterLocation.y+30).toString() + 'px';
-    fieldFilterElement.style.left=leftPx;
-    fieldFilterElement.style.top=topPx;
-    fieldFilterElement.style.display='block';
-}
-
 // Prepare the dropdown for use with Field Filter
-function prepareDropdown(fieldInput) {
+function prepareDropdown(fieldSelector) {
 
-    const dropdown = getDropdown(fieldInput);
+    const dropdown = getDropdown(fieldSelector);
     dropdown.buildDiv();
     const dropdownDiv = dropdown.div;
     const options = dropdownDiv.childNodes;
-
-    dropdownDiv.style.width='800px';
-
+    
     options.forEach((opt, index) => {
-        if (index==0) { return true; }
+        if (index==0) { 
+            opt.style.cssText='display:none;';
+        }
 
         const fieldId = dropdown.valueArray[index];
         const fieldName = dropdown.textArray[index];
@@ -131,68 +48,96 @@ function prepareDropdown(fieldInput) {
 
         let newFieldName = fieldName.replace(/\((Custom Body|Custom Column)\)/i,'');
         opt.innerHTML = `
-            <span style="width:35%;display: inline-block;">${newFieldName}</span>
-            <span style="width:25%;display: inline-block;">${fieldType}</span>
-            <span style="width:40%;display: inline-block;">${fieldId.substring(fieldId.indexOf('_') + 1)}</span>
+            <span class="ff_option" style="width:35%;">${newFieldName}</span>
+            <span class="ff_option" style="width:15%;">${fieldType}</span>
+            <span class="ff_option" style="width:15%;">${typeof rfTypes !== 'undefined' ? rfTypes[fieldId] : ''}</span>
+            <span class="ff_option" style="width:35%;">${fieldId.substring(fieldId.indexOf('_') + 1)}</span>
         `;
 
         return true;
     });
+    
+    dropdownDiv.style.width='800px';
+    dropdownDiv.id = `${fieldSelector.id}_dropdown`;
+
+    fieldSelector.setAttribute('dropdown',dropdownDiv.id);
+    fieldSelector.addEventListener('click',handleFieldSelectorClick);
+
+    const customBodyFieldsCheckbox = document.createElement('span');
+    const customColumnFieldsCheckbox = document.createElement('span');
+    const relatedTableFieldsCheckbox = document.createElement('span');
+    const nativeFieldsCheckbox = document.createElement('span');
+    const searchInputField = document.createElement('div');
+
+    const fieldFilter =  document.createElement('div');
+    fieldFilter.classList.add('ff_div');
+    fieldFilter.innerHTML = `<span class="ff_fieldspan"><input class="ff_checkbox" type="checkbox" id="ff_show_custom_body_fields" checked onpointerdown="event.preventDefault();" onclick="filterDropdowns(${dropdownDiv.id});"/> Custom Body Fields</span>`;
+    fieldFilter.innerHTML += `<span class="ff_fieldspan"><input class="ff_checkbox" type="checkbox" id="ff_show_custom_column_fields" checked onpointerdown="event.preventDefault();" onclick="filterDropdowns(${dropdownDiv.id});"/> Custom Column Fields</span>`;
+    fieldFilter.innerHTML += `<span class="ff_fieldspan"><input class="ff_checkbox" type="checkbox" id="ff_show_related_table_fields" checked onpointerdown="event.preventDefault();" onclick="filterDropdowns(${dropdownDiv.id});"/> Related Table Fields</span>`;
+    fieldFilter.innerHTML += `<span class="ff_fieldspan"><input class="ff_checkbox" type="checkbox" id="ff_show_native_fields" checked onpointerdown="event.preventDefault();" onclick="filterDropdowns(${dropdownDiv.id});"/> Native Fields</span>`;
+    fieldFilter.innerHTML += `<span class="ff_fieldspan"><input class="ff_textbox" type="text" id="ff_show_search_input" value="" onpointerdown="event.preventDefault();this.focus();" onkeydown="event.stopImmediatePropagation();" onkeypress="event.stopImmediatePropagation();" onkeyup="event.stopImmediatePropagation(); filterDropdowns(${dropdownDiv.id});" onfocus=" event.preventDefault();"></span>`;
+
+    dropdownDiv.insertBefore(fieldFilter,dropdownDiv.childNodes[1]);
 
 }
 
+// Automatically scroll the div window to account for the filter settings element. Otherwise, selected field gets hidden behind it.
+function handleFieldSelectorClick(event) {
+    try {        
+        const dropdown = getDropdown(event.target);
+        if (dropdown.currentCell) {
+            const currentSelectionLocation = dropdown.currentCell.getBoundingClientRect();;
+            dropdown.div.scrollBy({
+                top: -40,
+                behavior: 'instant'
+            });
+        }
+    } catch (err) {
+        console.error(`Error occurred while trying to find current selection for scrolling: ${err}`);
+    }   
+}
+
 // Show or hide options based on current Field Filter selections
-function filterDropdowns () {
+function filterDropdowns (dropdownDiv) {
 
     const showCustomBodyFields = document.getElementById('ff_show_custom_body_fields');
     const showRelatedTableFields = document.getElementById('ff_show_related_table_fields');
     const showCustomColumnFields = document.getElementById('ff_show_custom_column_fields');
     const showNativeFields = document.getElementById('ff_show_native_fields');
     const searchInputField = document.getElementById('ff_show_search_input');
-    
-    document.querySelectorAll(fieldsToFilter).forEach((el) => {
-        const dropdown = getDropdown(el);
-        const dropdownDiv = dropdown.div;
-        const options = dropdownDiv.childNodes;
-        
-        if (!showCustomColumnFields.checked
-            || !showCustomBodyFields.checked
-            || !showRelatedTableFields.checked
-            || !showNativeFields.checked
-            || searchInputField.value
-        ) {
-            options[0].textContent = 'Note - This list is being filtered by Field Filter';
-            options[0].style.fontWeight='bold';
-        } else {
-            options[0].textContent = '';
+
+    const options = dropdownDiv.childNodes;
+
+    options.forEach((opt, index) => {
+
+        if (index == 0) {
+            return true;
         }
 
-        options.forEach((opt, index) => {
-            // Filter by field type
-            switch(opt.getAttribute('ff_fieldtype')) {
-                case 'Related Field':
-                    opt.style.display = showRelatedTableFields.checked ? 'block' : 'none';
-                    break;
-                case 'Custom Body':
-                    opt.style.display = showCustomBodyFields.checked ? 'block' : 'none';
-                    break;
-                case 'Custom Column':
-                    opt.style.display = showCustomColumnFields.checked ? 'block' : 'none';
-                    break;
-                case 'Native Field':
-                    opt.style.display = showNativeFields.checked ? 'block' : 'none';
-                    break;
-            };
-            // Filter by field Name or field ID
-            if (searchInputField.value 
-                && opt.style.display == 'block'
-                && !opt.textContent.toLowerCase().includes(searchInputField.value.toLowerCase())) {
-                opt.style.display = 'none';
-            }
-            return true;
-        });
-
+        // Filter by field type
+        switch(opt.getAttribute('ff_fieldtype')) {
+            case 'Related Field':
+                opt.style.display = showRelatedTableFields.checked ? 'block' : 'none';
+                break;
+            case 'Custom Body':
+                opt.style.display = showCustomBodyFields.checked ? 'block' : 'none';
+                break;
+            case 'Custom Column':
+                opt.style.display = showCustomColumnFields.checked ? 'block' : 'none';
+                break;
+            case 'Native Field':
+                opt.style.display = showNativeFields.checked ? 'block' : 'none';
+                break;
+        };
+        // Filter by field Name or field ID
+        if (searchInputField.value 
+            && opt.style.display == 'block'
+            && !opt.textContent.toLowerCase().includes(searchInputField.value.toLowerCase())) {
+            opt.style.display = 'none';
+        }
         return true;
     });
+
+    return true;
 
 }
