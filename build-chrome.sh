a1ad59d0ab7ab265642ec9d8d06a190d46986d8c
Eric Lackey <eric.lackey@daveramsey.com>
2023-12-29 08:06:27 -0500

    Add popup html and image

--- build-chrome.sh
+++ build-chrome.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 echo "Building Chrome version $1"
-
 mkdir -p versions/fieldfinder_0_$1/chrome/build
 cp -r css versions/fieldfinder_0_$1/chrome/build
 cp -r scripts versions/fieldfinder_0_$1/chrome/build
+cp popup.html versions/fieldfinder_0_$1/chrome/build
+cp icon128.png versions/fieldfinder_0_$1/chrome/build
 cp manifest_chrome.json versions/fieldfinder_0_$1/chrome/build/manifest.json
 cd versions/fieldfinder_0_$1/chrome/build/
 zip -r ../../fieldfinder_0_$1_chrome.zip *
